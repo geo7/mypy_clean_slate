cd5a5453a7acea3872314e765af85ca0b68409b6
geo7 <georgelenton@gmail.com>
2021-07-12 12:20:25 +0100

    update version

--- tests/test_mypy_clean_slate.py
+++ tests/test_mypy_clean_slate.py
@@ -7,7 +7,7 @@ from mypy_clean_slate import __version__, main
 
 
 def test_version() -> None:
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.1.1"
 
 
 def test_mypy_clean_slate_usage() -> None:
