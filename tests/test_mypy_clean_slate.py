c8b8969a9cac90a82f120f92d7e625a1161a674b
geo7 <georgelenton@gmail.com>
2021-08-22 18:38:15 +0100

    update example, use pytest tmp_path

--- tests/test_mypy_clean_slate.py
+++ tests/test_mypy_clean_slate.py
@@ -7,10 +7,10 @@ from mypy_clean_slate import __version__, main
 
 
 def test_version() -> None:
-    assert __version__ == "0.1.3"
+    assert __version__ == "0.1.4"
 
 
-def test_mypy_clean_slate_usage() -> None:
+def test_mypy_clean_slate_usage(tmp_path: pathlib.Path) -> None:
     # atm this is a pretty broad usage test - just checks that things are, pretty much,
     # working as expected.
     py_file_before_fix = textwrap.dedent(
@@ -39,15 +39,15 @@ def test_mypy_clean_slate_usage() -> None:
     from __future__ import annotations
 
 
-    def add(*, arg_1, arg_2): # type: ignore[no-untyped-def]
+    def add(*, arg_1, arg_2):  # type: ignore[no-untyped-def]
         return arg_1 + arg_2
 
 
-    add(arg_1=1, arg_2="s") # type: ignore[no-untyped-call]
+    add(arg_1=1, arg_2="s")  # type: ignore[no-untyped-call]
 
 
-    def useless_sub(*, arg_1: float, arg_2: Sequence): # type: ignore[no-untyped-def, name-defined]
-        return add(arg_1=arg_1, arg_2="what") - arg_2 # type: ignore[no-untyped-call]
+    def useless_sub(*, arg_1: float, arg_2: Sequence):  # type: ignore[no-untyped-def, name-defined]
+        return add(arg_1=arg_1, arg_2="what") - arg_2  # type: ignore[no-untyped-call]
 
 
     useless_sub(arg_1=3, arg_2=4)
@@ -55,16 +55,12 @@ def test_mypy_clean_slate_usage() -> None:
     """
     ).strip()
 
-    python_file = pathlib.Path("file_to_check.py")
+    python_file = pathlib.Path(tmp_path, "file_to_check.py")
     python_file.write_text(py_file_before_fix)
 
     # there's probably a much nicer way to write these tests.
-    report_output = pathlib.Path("testing_report_output.txt")
-    report_output.write_text(main.generate_mypy_error_report())
+    report_output = pathlib.Path(tmp_path, "testing_report_output.txt")
+    report_output.write_text(main.generate_mypy_error_report(path_to_code=python_file))
 
     main.add_type_ignores(report_output=report_output)
     assert python_file.read_text().strip() == py_file_after_fix
-
-    # clean up
-    report_output.unlink()
-    python_file.unlink()
