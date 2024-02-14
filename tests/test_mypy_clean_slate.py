ca398de77d51111c523d63a1881159609bf6668c
geo7 <geo7@users.noreply.github.com>
2024-02-14 14:11:43 +0000

    fix: ensure duplicate codes aren't added to ignores, sort output (#95)
    Ensures that ignores such as
    
    ```
    type: ignore[x, x]
    ```
    
    Aren't created as a result of the same error happening multiple times on
    the same line.
    
    Whilst changing this behaviour the output was sorted - meaning that
    previous ignores added as:
    
    ```
    ignore[y, x]
    ```
    
    Are now (consistently) added in alphabetical order as:
    
    ```
    ignore[x, y]
    ```
    
    see: https://github.com/geo7/mypy_clean_slate/issues/89 for original
    issue.

--- tests/test_mypy_clean_slate.py
+++ tests/test_mypy_clean_slate.py
@@ -50,7 +50,7 @@ def add(*, arg_1, arg_2):  # type: ignore[no-untyped-def]
 add(arg_1=1, arg_2="s")   # type: ignore[no-untyped-call] # inline comment.
 
 
-def useless_sub(*, arg_1: float, arg_2: Sequence):  # type: ignore[no-untyped-def, name-defined]
+def useless_sub(*, arg_1: float, arg_2: Sequence):  # type: ignore[name-defined, no-untyped-def]
     return add(arg_1=arg_1, arg_2="what") - arg_2  # type: ignore[no-untyped-call]
 
 
@@ -63,6 +63,37 @@ useless_sub(arg_1=3, arg_2="4")
     python_file.write_text(py_file_before_fix, encoding="utf8")
 
     # there's probably a much nicer way to write these tests.
+    report_output = pathlib.Path(tmp_path, "testing_report_output.txt")
+    report_output.write_text(
+        main.generate_mypy_error_report(path_to_code=python_file),
+        encoding="utf8",
+    )
+
+    main.add_type_ignores(report_output=report_output)
+    assert python_file.read_text(encoding="utf8").strip() == py_file_after_fix
+
+
+def test_no_duplicate_codes_added(tmp_path: pathlib.Path) -> None:
+    """Ensure duplicate ignore messages aren't applied."""
+    py_file_before_fix = textwrap.dedent(
+        """
+    from __future__ import annotations
+
+    object().foo, object().bar
+    """,
+    ).strip()
+
+    py_file_after_fix = textwrap.dedent(
+        """
+    from __future__ import annotations
+
+    object().foo, object().bar  # type: ignore[attr-defined]
+    """
+    ).strip()
+
+    python_file = pathlib.Path(tmp_path, "file_to_check.py")
+    python_file.write_text(py_file_before_fix, encoding="utf8")
+
     report_output = pathlib.Path(tmp_path, "testing_report_output.txt")
     report_output.write_text(
         main.generate_mypy_error_report(path_to_code=python_file),
